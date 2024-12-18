32328897fd88fc95f23bc0043058ef7e4566e0f2
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2024-12-18 12:58:48 +0200

    implimented backend data base to a website

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,11 +1,23 @@
+import ProductsView from "@/components/ProductsView";
 import { Button } from "@/components/ui/button";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+	const products = await getAllProducts();
+
+	// console.log(
+	// 	crypto.randomUUID().slice(0, 5) +
+	// 	`>>> Rendered the home page cache with ${products.length} products and ${categories.length} categories`
+	// );
+
   return (
     <div>
       <h1>Test </h1>
-			<Button>Click me</Button>
+
+			<div className="flex flex-col items-center justify-top min-h-screen bg-gray-100p-4">
+				<ProductsView products = {products}/>
+			</div>
     </div>
   );
 }
