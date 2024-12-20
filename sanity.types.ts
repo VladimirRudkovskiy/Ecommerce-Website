231b7f8106e42df5f908f8d47adacccc6a7528f1
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2024-12-20 14:31:50 +0200

    implimenting search query with search params

--- sanity.types.ts
+++ sanity.types.ts
@@ -356,6 +356,69 @@ export type ALL_PRODUCTS_QUERYResult = Array<{
   stock?: number;
 }>;
 
+// Source: ./sanity/lib/products/searchProductsByName.ts
+// Variable: PRODUCT_SEARCH_QUERY
+// Query: *[			_type == "product"			&& name match $searchParam		] | order(name asc)
+export type PRODUCT_SEARCH_QUERYResult = Array<{
+  _id: string;
+  _type: "product";
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  name?: string;
+  slug?: Slug;
+  image?: {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: "image";
+  };
+  description?: Array<{
+    children?: Array<{
+      marks?: Array<string>;
+      text?: string;
+      _type: "span";
+      _key: string;
+    }>;
+    style?: "blockquote" | "h1" | "h2" | "h3" | "h4" | "normal";
+    listItem?: "bullet";
+    markDefs?: Array<{
+      href?: string;
+      _type: "link";
+      _key: string;
+    }>;
+    level?: number;
+    _type: "block";
+    _key: string;
+  } | {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    alt?: string;
+    _type: "image";
+    _key: string;
+  }>;
+  price?: number;
+  categories?: Array<{
+    _ref: string;
+    _type: "reference";
+    _weak?: boolean;
+    _key: string;
+    [internalGroqTypeReferenceTo]?: "category";
+  }>;
+  stock?: number;
+}>;
+
 // Source: ./sanity/lib/sales/getActiveSaleByCouponCode.ts
 // Variable: ACTIVE_SALE_BY_COUPON_QUERY
 // Query: *[			_type == 'sale'			&& isActive == true			&& couponCode == $couponCode		] | order(validFrom desc)[0]
@@ -380,6 +443,7 @@ declare module "@sanity/client" {
   interface SanityQueries {
     "\n\t\t*[_type == \"category\"] | order(name asc)\n\t\t": ALL_CATEGORIES_QUERYResult;
     "\n\t\t*[\n\t\t\t_type == \"product\"\n\t\t] | order(name asc)\n\t\t": ALL_PRODUCTS_QUERYResult;
+    "\n\t\t*[\n\t\t\t_type == \"product\"\n\t\t\t&& name match $searchParam\n\t\t] | order(name asc)\n\t\t": PRODUCT_SEARCH_QUERYResult;
     "\n\t\t*[\n\t\t\t_type == 'sale'\n\t\t\t&& isActive == true\n\t\t\t&& couponCode == $couponCode\n\t\t] | order(validFrom desc)[0]\n\t\t": ACTIVE_SALE_BY_COUPON_QUERYResult;
   }
 }
