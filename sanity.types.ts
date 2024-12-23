fa4cf5e92947fdd18a68724561ae193bd793a169
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2024-12-23 13:23:13 +0200

    implimented fetching categories by id

--- sanity.types.ts
+++ sanity.types.ts
@@ -419,6 +419,69 @@ export type PRODUCT_BY_ID_QUERYResult = {
   stock?: number;
 } | null;
 
+// Source: ./sanity/lib/products/getProductsByCategory.tsx
+// Variable: PRODUCTS_BY_CATEGORY_QUERY
+// Query: *[			_type == "product"			&& references(*[_type == "category" && slug.current == $categorySlug].id)		] | order(name asc)
+export type PRODUCTS_BY_CATEGORY_QUERYResult = Array<{
+  _id: string;
+  _type: "product";
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  name?: string;
+  slug?: Slug;
+  image?: {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: "image";
+  };
+  description?: Array<{
+    children?: Array<{
+      marks?: Array<string>;
+      text?: string;
+      _type: "span";
+      _key: string;
+    }>;
+    style?: "blockquote" | "h1" | "h2" | "h3" | "h4" | "normal";
+    listItem?: "bullet";
+    markDefs?: Array<{
+      href?: string;
+      _type: "link";
+      _key: string;
+    }>;
+    level?: number;
+    _type: "block";
+    _key: string;
+  } | {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    alt?: string;
+    _type: "image";
+    _key: string;
+  }>;
+  price?: number;
+  categories?: Array<{
+    _ref: string;
+    _type: "reference";
+    _weak?: boolean;
+    _key: string;
+    [internalGroqTypeReferenceTo]?: "category";
+  }>;
+  stock?: number;
+}>;
+
 // Source: ./sanity/lib/products/searchProductsByName.ts
 // Variable: PRODUCT_SEARCH_QUERY
 // Query: *[			_type == "product"			&& name match $searchParam		] | order(name asc)
@@ -507,6 +570,7 @@ declare module "@sanity/client" {
     "\n\t\t*[_type == \"category\"] | order(name asc)\n\t\t": ALL_CATEGORIES_QUERYResult;
     "\n\t\t*[\n\t\t\t_type == \"product\"\n\t\t] | order(name asc)\n\t\t": ALL_PRODUCTS_QUERYResult;
     "\n\t\t\t*[\n\t\t\t\t_type == \"product\" && slug.current == $slug\n\t\t\t] | order(name asc) [0]\n\t\t\t": PRODUCT_BY_ID_QUERYResult;
+    "\n\t\t*[\n\t\t\t_type == \"product\"\n\t\t\t&& references(*[_type == \"category\" && slug.current == $categorySlug].id)\n\t\t] | order(name asc)\n\t\t": PRODUCTS_BY_CATEGORY_QUERYResult;
     "\n\t\t*[\n\t\t\t_type == \"product\"\n\t\t\t&& name match $searchParam\n\t\t] | order(name asc)\n\t\t": PRODUCT_SEARCH_QUERYResult;
     "\n\t\t*[\n\t\t\t_type == 'sale'\n\t\t\t&& isActive == true\n\t\t\t&& couponCode == $couponCode\n\t\t] | order(validFrom desc)[0]\n\t\t": ACTIVE_SALE_BY_COUPON_QUERYResult;
   }
